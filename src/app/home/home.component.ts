c5fab4e2fda205fef631edb54535aa46de14cb5a
jardenoh <jaredpardeneaux@gmail.com>
2025-04-06 23:28:58 -0500

    Additional home page changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { SearchbarComponent } from '../searchbar/searchbar.component';  // Import the SearchbarComponent
+import { SearchbarComponent } from '../searchbar/searchbar.component';
 
 @Component({
   selector: 'app-home',
-  standalone: true,  // Mark as standalone component
-  imports: [SearchbarComponent],  // Include SearchbarComponent in imports
+  standalone: true,
+  imports: [SearchbarComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  // Home component logic
+  images: string[] = [
+    'assets/gpu.png',
+    'assets/cpu.png',
+    'assets/motherboard.png',
+    'assets/ram.png',
+    'assets/storage.png',
+    'assets/psu.png',
+    'assets/cooler.png'
+  ];
+
+  currentIndex: number = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000); // rotate every 3 seconds
+  }
 }
